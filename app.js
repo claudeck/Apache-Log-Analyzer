7ea98daf992f196cec533715a702e17dc5125a26
Administrator <administrator@ubuntu.ubuntu-domain>
2012-01-10 23:13:27 +0800

    add upload file function

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes')
+var routes = require('./routes');
 
-var app = module.exports = express.createServer();
+var form = require('connect-form');
+
+delete express.bodyParser.parse['multipart/form-data'];
+
+var app = module.exports = express.createServer(
+    form({ keepExtensions: true })
+);
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({secret: 'keyboard cat'}));
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+app.configure('production', function () {
+    app.use(express.errorHandler());
 });
 
 // Routes
 
 app.get('/', routes.index);
 app.get('/upload', routes.upload);
+app.post('/upload', routes.uploadLogFile);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,24 @@ exports.index = function (req, res) {
 };
 
 exports.upload = function (req, res) {
-    res.render('upload', {title:'Upload', activeMenu:'Upload'})
+    var message = req.flash('info');
+    res.render('upload', {title:'Upload', activeMenu:'Upload', message : message})
 };
+
+exports.uploadLogFile = function(req, res, next){
+    req.form.complete(function(err, fields, files){
+        if(err){
+            next(err);
+        } else {
+            console.log('\nuploaded %s to %s', files.logFile.filename, files.logFile.path);
+            req.flash('info', 'uploaded ' + files.logFile.filename + ' to ' + files.logFile.path);
+            res.redirect('back');
+        }
+    });
+    /*
+    req.form.on('progress', function(bytesReceived, bytesExpected){
+        var percent = (bytesReceived / bytesExpected * 100) | 0;
+        process.stdout().write('Uploading: %' + percent + '\r');
+    });
+    */
+}
